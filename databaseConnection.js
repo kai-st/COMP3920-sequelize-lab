63d24752bf48f1a70af2de6c9d79cf0b2259dd51
Kai Steingarten <ksteingarten@my.bcit.ca>
2026-03-10 19:29:04 -0700

    Use port in database connection

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,16 +1,16 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const dbConfigLocal = {
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	multipleStatements: false,
-	namedPlaceholders: true
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: false,
+  namedPlaceholders: true,
 };
 
 var database = mysql.createPool(dbConfigLocal);
 
 module.exports = database;
-		
